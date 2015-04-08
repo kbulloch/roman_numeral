63fc4829d89c2b41f1de5b2b441e37095c79e687
David Eliason & Kyle Bulloch <student@epicodus.com>
2015-04-08 13:29:34 -0700

    Add ability to parse all dollar denominations

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,47 @@
 var coin_combo = function(input) {
+    var change = input * 100;
+
     var penny = 0;
     var nickel = 0;
     var dime = 0;
     var quarter = 0;
+    var dollar = 0;
+    var five = 0;
+    var ten = 0;
+    var twenty = 0;
+    var fifty = 0;
+    var hundred = 0;
 
-    var change = input * 100;
-
+    while((change - 10000) >= 0)
+    {
+        hundred++;
+        change-= 10000;
+    }
+    while((change - 5000) >= 0)
+    {
+        fifty++;
+        change-= 5000;
+    }
+    while((change - 2000) >= 0)
+    {
+        twenty++;
+        change-= 2000;
+    }
+    while((change - 1000) >= 0)
+    {
+        ten++;
+        change-= 1000;
+    }
+    while ((change - 500) >= 0)
+    {
+        five++;
+        change -= 500;
+    }
+    while ((change - 100) >= 0)
+    {
+        dollar++;
+        change -= 100;
+    }
     while ((change - 25) >= 0)
     {
         quarter++;
@@ -26,7 +62,7 @@ var coin_combo = function(input) {
     penny = Math.round(change);
     //used round because of 2.000000001 pennies being returned on 0.07
 
-    return [penny, nickel, dime, quarter];
+    return [penny, nickel, dime, quarter, dollar, five, ten, twenty, fifty, hundred];
 };
 
 $(document).ready(function() {
@@ -41,13 +77,26 @@ $(document).ready(function() {
     var nickel = result[1];
     var dime = result[2];
     var quarter = result[3];
+    var dollar = result[4];
+    var five = result[5];
+    var ten = result[6];
+    var twenty = result[7];
+    var fifty = result[8];
+    var hundred = result[9];
 
 
     $('#change_returned').append(
         "<li class='text-primary'>Pennies: " + penny + "</li>" +
         "<li class='text-success'>Nickles: " + nickel + "</li>" +
         "<li class='text-danger'>Dimes: " + dime + "</li>" +
-        "<li class='text-warning'>Quarters: " + quarter + "</li>");
+        "<li class='text-warning'>Quarters: " + quarter + "</li>" +
+        "<li class='text-warning'>Dollars: " + dollar + "</li>" +
+        "<li class='text-warning'>Fives: " + five + "</li>" +
+        "<li class='text-success'>Tens: " + ten + "</li>" +
+        "<li class='text-success'>Twenties: " + twenty + "</li>" +
+        "<li class='text-success'>Fifties: " + fifty + "</li>" +
+        "<li class='text-success'>Hundreds: " + hundred + "</li>");
+
 
     $("#change_returned").show();
     event.preventDefault();
