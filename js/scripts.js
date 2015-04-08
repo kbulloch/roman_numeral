56a88b77c60c5d11313cb92bb677925f4ab4bf1a
David Eliason & Kyle Bulloch <student@epicodus.com>
2015-04-08 15:20:47 -0700

    one thru six working

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,89 @@
 var roman = function(input) {
-    return "I";
+
+    var input1 = parseInt(input);
+    var output = "";
+
+    if (input1 > 3999)
+    {
+        return "That number is too big to be converted!";
+    }
+
+    else
+    {
+
+
+        // possible structure for input numbers > 9
+        // var split_input = input.split("");
+        //
+        // for(var i = 0; i< split_input.length; i++)
+        // {
+        //     if([split_input.length-1] === 1)
+        //     {
+        //         output += "I";
+        //     }
+        // }
+        //
+
+
+
+        if(input1 < 4)
+        {
+            for(var i = 0; i < input1; i++)
+            {
+                output += "I";
+            }
+
+        }
+
+        else if(input1 % 5 === 4)
+        {
+            output += "I";
+            input1 -= 4;
+
+            if(input1/5 === 0)
+            {
+                output += "V";
+            }
+        }
+
+        else if(input1 % 5 === 0)
+        {
+            output += "V";
+        }
+
+        else if(input1 % 5 === 1)
+        {
+            output += "VI";
+        }
+
+
+     }
+
+
+
+    return output;
 };
+
+// trying it out II
+
+
+// trying it out!
+
+// var roman = function(input) {
+//
+//     var input1 = parseInt(input);
+//
+//     if (input1 > 3999)
+//     {
+//         return "That number is too big to be converted!";
+//     }
+//
+//     else if(input1 % 5 === 0)
+//     {
+//         output += "V";
+//     }
+//     return output;
+// };
+
+//input = 2
+//output = "II"
--- spec/specs.js
+++ spec/specs.js
@@ -4,4 +4,24 @@ describe('roman', function() {
         expect(roman(1)).to.equal("I");
     });
 
+    it('will return the string "II" for the input number 2', function(){
+        expect(roman(2)).to.equal("II");
+    });
+
+    it('will return the string "III" for the input number 3', function(){
+        expect(roman(3)).to.equal("III");
+    });
+
+    it('will return the string "IV" for the input number 4', function(){
+        expect(roman(4)).to.equal("IV");
+    });
+
+    it('will return the string "V" for the input number 5', function(){
+        expect(roman(5)).to.equal("V");
+    });
+
+    it('will return the string "VI" for the input number 6', function(){
+        expect(roman(6)).to.equal("VI");
+    });
+
 });
